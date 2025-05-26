4d8cb28d078c3b0fccc434087d118022b4c6acfc
Paolo Medel <paolomedel17@gmail.com>
2025-05-26 19:18:55 -0400

    created an array to feed the sorts

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
+import java.lang.reflect.Array;
+
 public class Main {
     public static void main(String[] args){
-        System.out.println("hai");
+        int[] dataset =
+                {42, 7, 93, 18, 56, 81, 23, 66, 3, 77, 31, 10, 95, 2, 38, 84, 26, 59, 13, 99,
+                        5, 71, 48, 62, 20, 36, 89, 12, 68, 25, 53, 33, 90, 46, 74, 40, 9, 16, 64, 85, 29, 1, 55, 78, 14, 50, 34, 8, 97, 21};
     }
 }
